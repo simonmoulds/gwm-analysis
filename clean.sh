64a66f322f6168329cd80eec0aec996f542ca735
simonmoulds <sim.moulds@gmail.com>
2022-12-15 10:51:36 +0000

    add clean

---
+++ clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+module load Anaconda3/2020.11
+# module load NCO/5.0.1-foss-2021a
+source activate $DATA/envs/snakemake
+
+# PYTHONPATH sometimes causes issues
+export PYTHONPATH=
+
+echo "Any cleaning?"
+select yn in "None" "Logs and snakemake lock" "Logs only" "Lock only"; do
+    case $yn in
+        "None" ) break;;
+        "Logs and snakemake lock" ) snakemake --profile $HOME/.config/snakemake/slurm.arc --cores 1 --use-conda --conda-base-path $DATA --unlock; rm -f log/*.log; break;;
+        "Logs only" ) rm -f log/*.log; break;;
+        "Lock only" ) snakemake --profile $HOME/.config/snakemake/slurm.arc --cores 1 --use-conda --conda-base-path $DATA --unlock; break;;
+    esac
+done
+
+# snakemake --profile $HOME/.config/snakemake/slurm.arc --cores 1 --use-conda --conda-base-path $DATA --unlock
