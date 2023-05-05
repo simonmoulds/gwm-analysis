85977fba59d6a2a8a9025126a705164714219691
simonmoulds <sim.moulds@gmail.com>
2023-05-05 09:11:18 +0100

    debugging

--- workflow/scripts/make_land_frac.R
+++ workflow/scripts/make_land_frac.R
@@ -11,12 +11,12 @@ frac <- rast("resources/WFD-EI-LandFraction2d.nc")
 globe <- rast(nrow = 360, ncol = 720)
 
 ## Create extent for IGP
-igp_ext <- extent(x = 60, xmax = 100, ymin = 20, ymax = 40)
+igp_ext <- ext(x = 60, xmax = 100, ymin = 20, ymax = 40)
 
 ## Assign values, write output
 globe[] <- values(frac)
 igp <- globe %>% crop(igp_ext)
 writeRaster(
-    igp, "results/WFD-EI-LandFraction2d_IGP.tif",
-overwrite = TRUE
+  igp, "results/WFD-EI-LandFraction2d_IGP.tif",
+  overwrite = TRUE
 )
